9325917aa2e347855f98a23bfca1e37822f30764
ruben russel <rubenrussel@gmail.com>
2025-11-17 17:12:20 +0700

    FINAL FIX: Hapus PWABadge (Build) + Tambah vercel.json (CSP)

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -5,20 +5,20 @@ export const staticMovieData = [
     title: 'The Shawshank Redemption',
     description: 'Drama penjara yang diadaptasi dari novel Stephen King.',
     // FOTO BARU (dari TMDB - The Movie Database)
-    imageUrl: 'https://image.tmdb.org/t/p/w500/9O7gL8sPSVUTsih4GGuI9fD9L4P.jpg'
+    imageUrl: 'https://tse1.mm.bing.net/th/id/OIP.mA5SG8RpkLC4PvEhT45igAHaKr?rs=1&pid=ImgDetMain&o=7&rm=3'
   },
   {
     id: 2,
     title: 'The Godfather',
     description: 'Kisah klasik keluarga mafia Corleone di New York.',
     // FOTO BARU (dari TMDB - The Movie Database)
-    imageUrl: 'https://image.tmdb.org/t/p/w500/3bhkrj58VtuR1-Ibn8hZOS4LhQx.jpg'
+    imageUrl: 'https://tse2.mm.bing.net/th/id/OIP.9seaNpOEHh9zv-vdPkMCTgHaEK?w=800&h=450&rs=1&pid=ImgDetMain&o=7&rm=3'
   },
   {
     id: 3,
     title: 'The Dark Knight',
     description: 'Batman menghadapi musuh bebuyutannya, Joker.',
     // FOTO BARU (dari TMDB - The Movie Database)
-    imageUrl: 'https://image.tmdb.org/t/p/w500/8hfIqio2FPDroL4MyCgfgjI8I0.jpg'
+    imageUrl: 'https://th.bing.com/th/id/OIP.C58MrUTynA5UG12vuT_jwwHaLH?w=184&h=276&c=7&r=0&o=7&pid=1.7&rm=3'
   }
 ];
