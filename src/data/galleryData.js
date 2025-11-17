e3c69108507bf7fda39f28ea9e7885621202e601
ruben russel <82746213+rubenrussel@users.noreply.github.com>
2025-11-17 17:23:25 +0700

    Update galleryData.js

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -4,21 +4,18 @@ export const staticMovieData = [
     id: 1,
     title: 'The Shawshank Redemption',
     description: 'Drama penjara yang diadaptasi dari novel Stephen King.',
-    // FOTO BARU (dari TMDB - The Movie Database)
     imageUrl: 'https://tse1.mm.bing.net/th/id/OIP.mA5SG8RpkLC4PvEhT45igAHaKr?rs=1&pid=ImgDetMain&o=7&rm=3'
   },
   {
     id: 2,
     title: 'The Godfather',
     description: 'Kisah klasik keluarga mafia Corleone di New York.',
-    // FOTO BARU (dari TMDB - The Movie Database)
     imageUrl: 'https://tse2.mm.bing.net/th/id/OIP.9seaNpOEHh9zv-vdPkMCTgHaEK?w=800&h=450&rs=1&pid=ImgDetMain&o=7&rm=3'
   },
   {
     id: 3,
     title: 'The Dark Knight',
     description: 'Batman menghadapi musuh bebuyutannya, Joker.',
-    // FOTO BARU (dari TMDB - The Movie Database)
     imageUrl: 'https://th.bing.com/th/id/OIP.C58MrUTynA5UG12vuT_jwwHaLH?w=184&h=276&c=7&r=0&o=7&pid=1.7&rm=3'
   }
 ];
